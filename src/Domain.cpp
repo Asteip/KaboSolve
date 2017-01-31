a9f9f6fdf9ab67eb80afe203af7f9372ca786208
Asteip <abonnin44@gmail.com>
2017-01-31 18:02:14 +0100

    Un début compilable pour les domaines

--- src/Domain.cpp
+++ src/Domain.cpp
@@ -1,3 +1,8 @@
 #include "Domain.hpp"
 
 #include <iostream>
+
+Domain::Domain(int n, int *set) {
+	this->n = n;
+	this->set = set;
+}
--- src/Domain.hpp
+++ src/Domain.hpp
@@ -1,6 +1,15 @@
 #ifndef DOMAIN_HPP
 #define DOMAIN_HPP
 
-
+class Domain {
+	private:
+		int n;
+		int size;
+		int *set;		// Séparé en deux [valeurs prunées / valeurs possibles]
+		int *indexes;	// [Indices de la variable qui a imposé le prunage / ignoré]
+		
+	public:
+		Domain(int n, int *set);
+};
 
 #endif
