77ac054d4d647d4570a24101a4f0e8d89887f471
--global <--global>
2017-02-15 16:40:31 +0100

    ça compile, on fait un truc bien :p

--- src/solver/problems/pNQueen.cpp
+++ src/solver/problems/pNQueen.cpp
@@ -1,22 +1,26 @@
 #include "pNQueen.hpp"
+#include "../constraint.hpp"
+#include "../constraints/cNQueen.hpp"
+#include "../constraints/cAllDiff.hpp"
 
 void PNQueen::generateProblem() {
 	_domains = new Domain * [_n];
+	int *line;
 
 	for (int i = 0 ; i < _n ; ++i){
-		int line[_n];
+		line = new int [_n];
 		for (int j = 0 ; j < _n ; ++j){
 			line[j] = j;
 		}
-		_domains[i] = new Domain(i, _n, &line);
+		_domains[i] = new Domain(i, _n, line);
 	}
 
 	// Create the constraints
 	_m = 2;
 	_constraints = new Constraint * [2];
 
-	_constraints[0] = new cNQueen(_domains, _n);
-	_constraints[1] = new cAllDiff(_domains, _n);
+	_constraints[0] = new CNQueen(_domains, _n);
+	_constraints[1] = new CAllDiff(_domains, _n);
 }
 
 PNQueen::PNQueen(int n){
@@ -28,7 +32,7 @@ PNQueen::PNQueen(int n){
 
 PNQueen::~PNQueen() {}
 
-Domains * PNQueen::getMinDomain(){
+Domain * PNQueen::getMinDomain(){
 	Domain * res = _domains[0];
 	int i = 1;
 	int min = res->getSize();
