5a8b8bc5f7c4e6dbc276e00551f25d847421648a
Alexis <abonnin44@gmail.com>
2017-03-22 17:44:03 +0100

    add for merge

--- src/solver/main.cpp
+++ src/solver/main.cpp
@@ -22,34 +22,41 @@ int main(int argc, char **argv) {
 	//srand(0);
 	clock_t debut, fin;
 
+	char n_queens[] = "n-queens";
+	char more_money[] = "more-money";
+	char magic_square[] = "magic-square";
+	char sudoku[] = "sudoku";
+
 	if(argc < 3){
-		cout << "Usage : KaboSolve <probleme> <choix-solver> [<options>]" << endl;
-		cout << "Options valides : " << endl;
-		cout << "	probleme : N-reines, more-money, magic-square" << endl;
-		cout << "	choix-solver : one, all" << endl;
-		cout << "	options : options complémentaires" << endl;
+		cout << "Usage : KaboSolve <problem> <number of solutions> [<options>]" << endl;
+		cout << "Valid options : " << endl;
+		cout << "	problem : n-queens, more-money, magic-square, sudoku" << endl;
+		cout << "	number of solutions : \"one\" (finds the first solution) or \"all\" (finds all solutions)" << endl;
+		cout << "	options : additional options" << endl;
 	}
 	else{
-		cout << "Démarrage du solver..." << endl << endl;
+		cout << "======== Starting ========" << endl << endl;
 
-		if(strstr(argv[1], "N-reines")){
+		if(strstr(argv[1], n_queens) && strlen(argv[1]) == strlen(n_queens)){
 
-			cout << "***** Problème du N-reines. *****" << endl << endl;
+			cout << "***** N-QUEENS problem *****" << endl << endl;
 
-			if(argv[3] == NULL || strlen(argv[3]) == 0){
-				cout << "Veuillez spécifier un nombre de reines." << endl;
-				cout << "Exemple : \"KaboSolve N-reines one 5\" pour exécuter le problème sur 5 reines." << endl;
+			if(argc <= 3){
+				cout << "Usage : KaboSolve n-queens <number of solutions> <number of queens>" << endl;
+				cout << "	number of solutions : \"one\" (finds the first solution) or \"all\" (finds all solutions)" << endl;
+				cout << "	number of queens : number of queens you want to run the solver on" << endl;
+				cout << endl << "Example : \"KaboSolve n-queens all 5\" to find all the solution of the n-queens problem for 5 queens" << endl;
 			}
 			else{
 				int N = atoi(argv[3]);
 				
-				if(N == 0){
-					cout << "ERREUR : le nombre de reines doit être un entier" << endl;
+				if(N <= 0){
+					cout << "ERROR : the number of queens must be an integer greater than 0" << endl;
 				}
 				else{
 					Problem *p = new PNQueen(N);
-
 					Solver s(p);
+					
 					debut = clock();
 
 					if(strstr(argv[2], "one"))
@@ -57,21 +64,20 @@ int main(int argc, char **argv) {
 					else if(strstr(argv[2], "all"))
 						s.solveAll();
 					else
-						cout << "ERREUR : ce choix de solver n'existe pas." << endl;
+						cout << "ERROR : <number of solution> parameter accepts only \"one\" or \"all\" value" << endl;
 
 					fin = clock();
-
-					cout << endl << "temps : " << (double)(fin-debut)/CLOCKS_PER_SEC << "s" << endl;
+					cout << endl << "Time : " << (double)(fin-debut)/CLOCKS_PER_SEC << "s" << endl;
 				}
 			}
 
 		}
-		else if(strstr(argv[1], "more-money")){
-			cout << "***** Problème du send more money. *****" << endl << endl;;
+		else if(strstr(argv[1], more_money) && strlen(argv[1]) == strlen(more_money)){
+			cout << "***** SEND MORE MONEY problem *****" << endl << endl;
 				
 			Problem *p = new PMoreMoney();
-			
 			Solver s(p);
+
 			debut = clock();
 
 			if(strstr(argv[2], "one"))
@@ -79,24 +85,26 @@ int main(int argc, char **argv) {
 			else if(strstr(argv[2], "all"))
 				s.solveAll();
 			else
-				cout << "ERREUR : ce choix de solver n'existe pas." << endl;
+				cout << "ERROR : <number of solution> parameter accepts only \"one\" or \"all\" value" << endl;
 
 			fin = clock();
 
-			cout << endl << "temps : " << (double)(fin-debut)/CLOCKS_PER_SEC << "s" << endl;
+			cout << endl << "Time : " << (double)(fin-debut)/CLOCKS_PER_SEC << "s" << endl;
 		}
-		else if(strstr(argv[1], "magic-square")){
-			cout << "***** Problème du carré magique. *****" << endl << endl;
+		else if(strstr(argv[1], magic_square) && strlen(argv[1]) == strlen(magic_square)){
+			cout << "***** MAGIC SQUARE problem *****" << endl << endl;
 
-			if(argv[3] == NULL || strlen(argv[3]) == 0){
-				cout << "Veuillez spécifier une taille de carré." << endl;
-				cout << "Exemple : \"KaboSolve magic-square one 5\" pour exécuter le problème sur un carré de taille 5." << endl;
+			if(argc <= 3){
+				cout << "Usage : KaboSolve magic-square <number of solutions> <size of the square>" << endl;
+				cout << "	number of solutions : \"one\" (finds the first solution) or \"all\" (finds all solutions)" << endl;
+				cout << "	size of the square : size of the square array" << endl;
+				cout << endl << "Example : \"KaboSolve magic-square all 5\" to find all the solution of the magic-square problem for a square of size 5x5" << endl;
 			}
 			else{
 				int N = atoi(argv[3]);
 				
-				if(N == 0){
-					cout << "ERREUR : la taille du carré doit être un entier" << endl;
+				if(N <= 0){
+					cout << "ERROR : the size of the square must be greater than 0" << endl;
 				}
 				else{
 					PMagicSquare *p = new PMagicSquare(N);
@@ -109,19 +117,38 @@ int main(int argc, char **argv) {
 					else if(strstr(argv[2], "all"))
 						s.solveAll();
 					else
-						cout << "ERREUR : ce choix de solver n'existe pas." << endl;
+						cout << "ERROR : <number of solution> parameter accepts only \"one\" or \"all\" value" << endl;
 
 					fin = clock();
 
-					cout << endl << "temps : " << (double)(fin-debut)/CLOCKS_PER_SEC << "s" << endl;
+					cout << endl << "Time : " << (double)(fin-debut)/CLOCKS_PER_SEC << "s" << endl;
 				}
 			}
 		}
+		else if(strstr(argv[1], sudoku) && strlen(argv[1]) == strlen(sudoku)){
+			cout << "***** SUDOKU problem *****" << endl << endl;
+				
+			/*Problem *p = new PMoreMoney();
+			Solver s(p);
+
+			debut = clock();
+
+			if(strstr(argv[2], "one"))
+				s.solve();
+			else if(strstr(argv[2], "all"))
+				s.solveAll();
+			else
+				cout << "ERROR : <number of solution> parameter accepts only \"one\" or \"all\" value" << endl;
+
+			fin = clock();
+
+			cout << endl << "Time : " << (double)(fin-debut)/CLOCKS_PER_SEC << "s" << endl;*/
+		}
 		else{
-			cout << "ERREUR : Pas de problème : " << argv[1] << endl << endl;
+			cout << "ERROR : the problem : " << argv[1] << " doesn't exist" << endl;
 		}
 
-		cout << endl << "=== END ===" << endl;
+		cout << endl << "======== END ========" << endl;
 	}
 
 	return 0;
